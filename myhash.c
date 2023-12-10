991e6d60381c3b16286c2c688298d0aee930dc68
KernelHC <rani.wishahy00@gmail.com>
2023-12-10 04:26:38 +0200

    Implemented HashNode functions

--- myhash.c
+++ myhash.c
@@ -1,5 +1,6 @@
 #include "myhash.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct hash_node {
     Key key;
@@ -14,12 +15,16 @@ struct hash_table {
 
 //******************************************** HashNode Implementation ***********************************************//
 
-HashNode createHashNode() {
-
+HashNode createHashNode(Key key, Value val) {
+    HashNode new_node = malloc(sizeof (struct hash_node));
+    new_node->key = key;
+    new_node->value = val;
+    new_node->next = NULL;
+    return new_node;
 }
 
 void destroyHashNode(HashNode hn) {
-
+    free(hn);
 }
 
 //******************************************** HashTable Implementation **********************************************//

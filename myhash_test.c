0bde059194cc1dfddf3063b809ac9f961fe60949
KernelHC <rani.wishahy00@gmail.com>
2023-12-11 14:19:38 +0200

    Initial myhash test

--- myhash_test.c
+++ myhash_test.c
@@ -3,7 +3,53 @@
 //
 #include "myhash.h"
 #include <stdio.h>
+#include <assert.h>
+
+void testInit() {
+    HashTable ht = createHashTable();
+    assert(!ht);
+    destroyHashtable(ht);
+}
+
+void testInsert() {
+
+}
+
+void testErase() {
+
+}
+
+void testSameKey() {
+
+}
+
+void testCopy() {
+
+}
+
+void testStress1() {
+
+}
+
+void testStress2() {
+
+}
+
+void (*tests[])() = {
+    testInit,
+    testInsert,
+    testCopy,
+    testSameKey,
+    testErase,
+    testStress1,
+    testStress2,
+    0
+};
 
 int main() {
+    for (int i = 0; tests[i] != 0; i++) {
+        tests[i]();
+        printf("Passed!\n");
+    }
     return 0;
 }
